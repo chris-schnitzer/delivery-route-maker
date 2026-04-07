5851a2c147f0356b86c3698b1010d62ca28f5988
chris-schnitzer <62219119+chris-schnitzer@users.noreply.github.com>
2026-04-07 17:27:10 +0100

    add new objects to vandata.js array and update positions

--- src/vanData.js
+++ src/vanData.js
@@ -1,5 +1,4 @@
 const vanData = [
-	// top field
 	{
 		vanNumber: 1,
 		position: 1
@@ -145,424 +144,452 @@ const vanData = [
 		position: 36
 	},
 	{
-		vanNumber: 158,
+		vanNumber: 153,
 		position: 37
 	},
 	{
-		vanNumber: 159,
-		position: 38
+		vanNumber: 152,
+		position:38
 	},
 	{
-		vanNumber: 160,
+		vanNumber: 158,
 		position: 39
 	},
 	{
-		vanNumber: 163,
+		vanNumber: 159,
 		position: 40
 	},
 	{
-		vanNumber: "39c",
+		vanNumber: 160,
 		position: 41
 	},
 	{
-		vanNumber: 40,
+		vanNumber: 163,
 		position: 42
 	},
 	{
-		vanNumber: 167,
+		vanNumber: "39c",
 		position: 43
 	},
 	{
-		vanNumber: 168,
+		vanNumber: 40,
 		position: 44
 	},
 	{
-		vanNumber: 29,
+		vanNumber: 167,
 		position: 45
 	},
 	{
-		vanNumber: 30,
+		vanNumber: 168,
 		position: 46
 	},
 	{
-		vanNumber: 82,
+		vanNumber: 29,
 		position: 47
 	},
 	{
-		vanNumber: 83,
+		vanNumber: 30,
 		position: 48
 	},
 	{
-		vanNumber: 84,
+		vanNumber: 82,
 		position: 49
 	},
 	{
-		vanNumber: 85,
+		vanNumber: 83,
 		position: 50
 	},
 	{
-		vanNumber: 86,
+		vanNumber: 84,
 		position: 51
 	},
 	{
-		vanNumber: 87,
+		vanNumber: 85,
 		position: 52
 	},
 	{
-		vanNumber: 89,
+		vanNumber: 86,
 		position: 53
 	},
 	{
-		vanNumber: 31,
+		vanNumber: 87,
 		position: 54
 	},
 	{
-		vanNumber: 81,
+		vanNumber: 89,
 		position: 55
 	},
 	{
-		vanNumber: 32,
+		vanNumber: 31,
 		position: 56
 	},
 	{
-		vanNumber: 80,
+		vanNumber: 81,
 		position: 57
 	},
 	{
-		vanNumber: 33,
+		vanNumber: 32,
 		position: 58
 	},
 	{
-		vanNumber: 79,
+		vanNumber: 80,
 		position: 59
 	},
 	{
-		vanNumber: 78,
+		vanNumber: 33,
 		position: 60
 	},
 	{
-		vanNumber: 42,
+		vanNumber: 79,
 		position: 61
 	},
 	{
-		vanNumber: 77,
+		vanNumber: 78,
 		position: 62
 	},
 	{
-		vanNumber: 76,
+		vanNumber: 42,
 		position: 63
 	},
 	{
-		vanNumber: 75,
+		vanNumber: 77,
 		position: 64
 	},
 	{
-		vanNumber: 74,
+		vanNumber: 43,
 		position: 65
 	},
 	{
-		vanNumber: 73,
+		vanNumber: 76,
 		position: 66
 	},
 	{
-		vanNumber: 45,
+		vanNumber: 75,
 		position: 67
 	},
 	{
-		vanNumber: 72,
-		position: 68
+		vanNumber: 44,
+		position:68
 	},
 	{
-		vanNumber: 47,
+		vanNumber: 74,
 		position: 69
 	},
 	{
-		vanNumber: 71,
+		vanNumber: 73,
 		position: 70
 	},
 	{
-		vanNumber: 48,
+		vanNumber: 46,
 		position: 71
 	},
 	{
-		vanNumber: 70,
+		vanNumber: 45,
 		position: 72
 	},
 	{
-		vanNumber: 69,
+		vanNumber: 72,
 		position: 73
 	},
 	{
-		vanNumber: 68,
+		vanNumber: 47,
 		position: 74
 	},
 	{
-		vanNumber: 67,
+		vanNumber: 71,
 		position: 75
 	},
 	{
-		vanNumber: 52,
+		vanNumber: 48,
 		position: 76
 	},
 	{
-		vanNumber: 66,
+		vanNumber: 70,
 		position: 77
 	},
 	{
-		vanNumber: 53,
+		vanNumber: 69,
 		position: 78
 	},
 	{
-		vanNumber: 65,
+		vanNumber: 68,
 		position: 79
 	},
 	{
-		vanNumber: 64,
+		vanNumber: 67,
 		position: 80
 	},
 	{
-		vanNumber: 63,
+		vanNumber: 52,
 		position: 81
 	},
 	{
-		vanNumber: 55,
+		vanNumber: 66,
 		position: 82
 	},
 	{
-		vanNumber: 56,
+		vanNumber: 53,
 		position: 83
 	},
 	{
-		vanNumber: 57,
+		vanNumber: 65,
 		position: 84
 	},
 	{
-		vanNumber: 58,
+		vanNumber: 54,
 		position: 85
 	},
 	{
-		vanNumber: 59,
+		vanNumber: 64,
 		position: 86
 	},
 	{
-		vanNumber: 60,
+		vanNumber: 63,
 		position: 87
 	},
 	{
-		vanNumber: 61,
+		vanNumber: 55,
 		position: 88
 	},
 	{
-		vanNumber: 62,
+		vanNumber: 56,
 		position: 89
 	},
 	{
-		vanNumber: 121,
+		vanNumber: 57,
 		position: 90
 	},
 	{
-		vanNumber: 122,
+		vanNumber: 58,
 		position: 91
 	},
 	{
-		vanNumber: 123,
+		vanNumber: 59,
 		position: 92
 	},
 	{
-		vanNumber: 118,
+		vanNumber: 60,
 		position: 93
 	},
 	{
-		vanNumber: 124,
+		vanNumber: 61,
 		position: 94
 	},
 	{
-		vanNumber: 117,
+		vanNumber: 62,
 		position: 95
 	},
 	{
-		vanNumber: 116,
+		vanNumber: 121,
 		position: 96
 	},
 	{
-		vanNumber: 125,
+		vanNumber: 122,
 		position: 97
 	},
 	{
-		vanNumber: 115,
+		vanNumber: 123,
 		position: 98
 	},
 	{
-		vanNumber: 126,
+		vanNumber: 118,
 		position: 99
 	},
 	{
-		vanNumber: 114,
+		vanNumber: 124,
 		position: 100
 	},
 	{
-		vanNumber: 127,
+		vanNumber: 117,
 		position: 101
 	},
 	{
-		vanNumber: 112,
+		vanNumber: 116,
 		position: 102
 	},
 	{
-		vanNumber: 111,
+		vanNumber: 125,
 		position: 103
 	},
 	{
-		vanNumber: 128,
+		vanNumber: 115,
 		position: 104
 	},
 	{
-		vanNumber: 129,
+		vanNumber: 126,
 		position: 105
 	},
 	{
-		vanNumber: 110,
+		vanNumber: 114,
 		position: 106
 	},
 	{
-		vanNumber: 109,
+		vanNumber: 127,
 		position: 107
 	},
 	{
-		vanNumber: 130,
+		vanNumber: 112,
 		position: 108
 	},
 	{
-		vanNumber: 108,
+		vanNumber: 111,
 		position: 109
 	},
 	{
-		vanNumber: 131,
+		vanNumber: 128,
 		position: 110
 	},
 	{
-		vanNumber: 107,
+		vanNumber: 129,
 		position: 111
 	},
 	{
-		vanNumber: 132,
+		vanNumber: 110,
 		position: 112
 	},
 	{
-		vanNumber: 106,
+		vanNumber: 109,
 		position: 113
 	},
 	{
-		vanNumber: 105,
+		vanNumber: 130,
 		position: 114
 	},
 	{
-		vanNumber: 133,
+		vanNumber: 108,
 		position: 115
 	},
 	{
-		vanNumber: 134,
+		vanNumber: 131,
 		position: 116
 	},
 	{
-		vanNumber: 104,
+		vanNumber: 107,
 		position: 117
 	},
 	{
-		vanNumber: 103,
+		vanNumber: 132,
 		position: 118
 	},
 	{
-		vanNumber: 135,
+		vanNumber: 106,
 		position: 119
 	},
 	{
-		vanNumber: 136,
+		vanNumber: 105,
 		position: 120
 	},
 	{
-		vanNumber: 102,
+		vanNumber: 133,
 		position: 121
 	},
 	{
-		vanNumber: 101,
+		vanNumber: 134,
 		position: 122
 	},
 	{
-		vanNumber: 100,
-		position: 123,
+		vanNumber: 104,
+		position: 123
 	},
 	{
-		vanNumber: 99,
+		vanNumber: 103,
 		position: 124
 	},
 	{
-		vanNumber: 139,
+		vanNumber: 135,
 		position: 125
 	},
 	{
-		vanNumber: 98,
+		vanNumber: 136,
 		position: 126
 	},
 	{
-		vanNumber: 140,
+		vanNumber: 102,
 		position: 127
 	},
 	{
-		vanNumber: 97,
-		position: 128
+		vanNumber: 137,
+		position:128
 	},
 	{
-		vanNumber: 96,
+		vanNumber: 101,
 		position: 129
 	},
 	{
-		vanNumber: 141,
-		position: 130
+		vanNumber: 100,
+		position: 130,
 	},
 	{
-		vanNumber: 142,
+		vanNumber: 99,
 		position: 131
 	},
 	{
-		vanNumber: 95,
+		vanNumber: 139,
 		position: 132
 	},
 	{
-		vanNumber: 143,
+		vanNumber: 98,
 		position: 133
 	},
 	{
-		vanNumber: 90,
+		vanNumber: 140,
 		position: 134
 	},
 	{
-		vanNumber: 94,
+		vanNumber: 97,
 		position: 135
 	},
 	{
-		vanNumber: 91,
+		vanNumber: 96,
 		position: 136
 	},
 	{
-		vanNumber: 93,
+		vanNumber: 141,
 		position: 137
 	},
 	{
-		vanNumber: 145,
+		vanNumber: 142,
 		position: 138
 	},
 	{
-		vanNumber: 147,
+		vanNumber: 95,
 		position: 139
 	},
 	{
-		vanNumber: 148,
+		vanNumber: 143,
 		position: 140
 	},
 	{
-		vanNumber: 149,
+		vanNumber: 90,
 		position: 141
+	},
+	{
+		vanNumber: 94,
+		position: 142
+	},
+	{
+		vanNumber: 91,
+		position: 143
+	},
+	{
+		vanNumber: 93,
+		position: 144
+	},
+	{
+		vanNumber: 145,
+		position: 145
+	},
+	{
+		vanNumber: 147,
+		position: 146
+	},
+	{
+		vanNumber: 148,
+		position: 147
+	},
+	{
+		vanNumber: 149,
+		position: 148
 	}
 ];
 
